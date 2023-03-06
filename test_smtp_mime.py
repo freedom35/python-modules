9c98ebdded290df8ab9f429210463a9f5e01428d
Alan <freedom480@gmail.com>
2023-03-06 10:59:49 -0800

    Updated comment

--- test_smtp_mime.py
+++ test_smtp_mime.py
@@ -37,7 +37,8 @@ def main():
         
         # Attachment note:
         # Specify full path if file to attach is not in local dir.
-        # Use os.path.join for platform (Windows/UNIX) independence (import os)
+        # Use os.path.join for platform (Windows/UNIX) independence 
+        # (import os)
         # i.e. if file is in resources sub-dir:
         # attachment_path = os.path.join('resources', 'readme.md')
 
