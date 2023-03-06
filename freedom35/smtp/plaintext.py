fdd7c31362602804e36102575418acad43e1f9f6
Alan <freedom480@gmail.com>
2023-03-06 10:55:33 -0800

    Updated code layout

--- freedom35/smtp/plaintext.py
+++ freedom35/smtp/plaintext.py
@@ -6,14 +6,14 @@
 # March 2023
 #######################################
 import smtplib
-
 from email.message import EmailMessage
 
 
 #######################################
 # Sends plaintext message using SMTP
 #######################################
-def send_plaintext_message(to_name, to_address, from_name, from_address, subject, content_as_string, smtp_user, smtp_pwd):
+def send_plaintext_message(to_name, to_address, from_name, from_address, 
+                           subject, content_as_string, smtp_user, smtp_pwd):
 
     # Create message
     msg = EmailMessage()
@@ -27,6 +27,7 @@ def send_plaintext_message(to_name, to_address, from_name, from_address, subject
 
     # Send using secure SMTP
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
-        # For security, login using an app generated password (not account password)
+        # For security, login using an app generated password 
+        # (not account password)
         server.login(smtp_user, smtp_pwd)
         server.send_message(msg)
